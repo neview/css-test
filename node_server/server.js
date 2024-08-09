49efda4247b36aa2b5f1540d047aeb2b5cc2f01b
芒果集团 <1282261308@qq.com>
2024-08-09 18:00:55 +0800

    '查看图片'

--- node_server/server.js
+++ node_server/server.js
@@ -8,6 +8,7 @@ app.get("/", (req, res) => {
   // res.sendFile(path.join(__dirname + "/flexible.html"));
   // res.sendFile(path.join(__dirname + "/css1.html"));
   res.sendFile(path.join(__dirname + "/touchscreenSlide.html"));
+  // res.sendFile(path.join(__dirname + "/scale.html"));
 });
 
 const port = 3000;
